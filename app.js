80b7313e45fd3114245174a91bb524e030bc9f45
Srimal Priyanga <s.priyanga22@gmail.com>
2015-10-20 10:31:55 +0530

    sequelize db package added

--- app.js
+++ app.js
@@ -36,6 +36,24 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+// a middleware with no mount path; gets executed for every request to the app
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now());
+  next();
+});
+
+// a middleware mounted on /user/:id; will be executed for any type of HTTP request to /user/:id
+app.use('/user/:id', function (req, res, next) {
+  console.log('Request Type:', req.method);
+  next();
+});
+
+// a route and its handler function (middleware system) which handles GET requests to /user/:id
+app.get('/user/:id', function (req, res, next) {
+  res.send('USER');
+});
+
+
 // error handlers
 
 // development error handler
